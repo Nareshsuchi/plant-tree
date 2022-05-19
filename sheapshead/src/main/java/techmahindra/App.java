34d68580a2fcca87935a967a1c26233eaf5e3572
Nareshsuchi <Cnareshdevops@gmail.com>
2022-05-19 15:53:59 +0000

    this is a maven commit

---
+++ sheapshead/src/main/java/techmahindra/App.java
@@ -0,0 +1,13 @@
+package techmahindra;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
